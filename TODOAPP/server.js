2ceedee48b97a8952d4b75a6d2274890e8eda65d
muse9312 <muse9312@naver.com>
2022-08-02 20:20:21 +0900

    html 데이터 서버에 post로 보내기

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -1,7 +1,12 @@
 // express 라이브러리 첨부
 const express = require('express');
+
 // 라이브러리 객체 생성
 const server = express();
+
+// html안에 적힌 데이터들을 해석 도와줌
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded({ extended: true }));
 
 // listen(서버띄울 포트번호, 띄운후 실행할 코드) => 8080 port로 서버만들어서 function()코드를 실행해주세요
 server.listen(8080, function () {
@@ -13,6 +18,8 @@ server.listen(8080, function () {
 // =================================================================
 
 // 누군가가 /pet 으로 방문을 하면 pet 관련된 안내문을 띄워주자
+// 함수 안에 함수 => 콜백함수
+// .get('경로', function (요청내용 , 응답할 방법) {})
 server.get('/pet', function (req, res) {
     res.send('펫용품 쇼핑 할 수 있는 페이지입니다')
 })
@@ -30,3 +37,17 @@ server.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html',)
 })
 
+server.get('/write', function (req, res) {
+    res.sendFile(__dirname + '/write.html',)
+})
+
+// 어떤 사람이 /add 경로로 post 요청을 하면... ??? 를 해주세요
+
+// post 요청으로 서버에 데이터 전송하고 싶으면
+// 1. body-parser 핋요
+// 2. form데이터 경우 input들에 name 쓰기 => 서버에서 input을 구분하기 위해 name = "이름" 쓰는것
+
+server.post('/add', function (req, res) {
+    res.send('전송 완료')
+    console.log(req.body);
+})
